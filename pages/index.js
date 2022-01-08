c93aacb0ed5388846c087bdb1dcae9e10e282020
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-08 15:53:35 +0100

    143 submiting data to API

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import {useRef} from 'react'; //referencje żeby pobrać dane z inputów
+import { useRef } from "react"; //referencje żeby pobrać dane z inputów
 
 function HomePage() {
   const emailInputRef = useRef();
@@ -10,16 +10,31 @@ function HomePage() {
     const enteredEmail = emailInputRef.current.value; //wartosc inputu
     const enteredFeedback = feedbackInputRef.current.value; //wartosc feedback
 
-    fetch(); //POST {email: 'te@te.com;, text: 'Some feedack'}
-
+    //POST {email: 'te@te.com;, text: 'Some feedack'}
+    const reqBody = {
+      email: enteredEmail,
+      text: enteredFeedback,
+    };
+    // nie trzeba pisac wąłsnej domeny
+    fetch("/api/feedback", {
+      method: "POST", //bardzo ważne, bo na to czeka endpoint
+      body: JSON.stringify(reqBody), // pamietać o konwersji z obiektu na jsona
+      headers: {
+        //explicit mówic że jsona wysyłam
+        "Content-Type": "application/json",
+      },
+    })//.then bo fetch zwraca Promise
+      .then((response) => response.json())
+      .then((data) => console.log(data)); //można coś zrobić ze zwrotką
   }
+
   return (
     <div>
       <h1>The Home Page</h1>
       <form onSubmit={submitFormHandler}>
         <div>
           <label htmlFor="email">Your Email Address</label>
-          <input type="email" id="email" ref={emailInputRef}/>
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div>
           <label htmlFor="feedback">Your feedback</label>
