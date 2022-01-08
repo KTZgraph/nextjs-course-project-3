24fdf195df0b0971ae29e857191c11044e145d30
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-08 15:30:14 +0100

    141 useRef() do danych z inputu

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
+import {useRef} from 'react'; //referencje żeby pobrać dane z inputów
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+    // można dodać walidacje, czy jest pusty input
+    const enteredEmail = emailInputRef.current.value; //wartosc inputu
+    const enteredFeedback = feedbackInputRef.current.value; //wartosc feedback
+
+
+  }
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef}/>
+        </div>
+        <div>
+          <label htmlFor="feedback">Your feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
